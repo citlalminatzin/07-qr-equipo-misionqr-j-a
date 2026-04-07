42eaa35db610231a16360932693212656925309e
Y_1DEz <90946049+lOriginalD@users.noreply.github.com>
2026-04-07 13:43:04 -0600

    Update qr.py

--- qr.py
+++ qr.py
@@ -6,8 +6,20 @@ Realiza la factorización QR de una matriz
 
 from gram_schmidt import gm, transpose, dot
 
-def qr(M:list[list[float]])->tuple[list[list[float]], list[list[float]]]:
-    """Realiza la factorización QR de una matriz M"""
-    Q = ...
-    R = ...
-    return Q,R
+ def qr(M: list[list[float]]) -> tuple[list[list[float]], list[list[float]]]:
+      """Realiza la factorización QR de una matriz M"""
+    
+    # Q como lista de columnas ortonormales
+    Q_cols = gm(M)
+    
+    # Convertimos Q a formato fila (matriz estándar)
+    Q = list(map(list, zip(*Q_cols)))
+    
+    # Transpuestas para trabajar por columnas
+    Qt = transpose(Q)
+    M_cols = transpose(M)
+    
+    # Construcción de R
+    R = [[dot(qi, aj) for aj in M_cols] for qi in Qt]
+    
+    return Q, R
